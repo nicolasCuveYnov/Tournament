7bde792d0df7ae9da99d3832b43979ab29511217
Nicolas <nicolas.cuve@ynov.com>
2021-11-19 12:51:02 +0100

    feat/ ajout route addEventToUser

--- routes/routes.js
+++ routes/routes.js
@@ -92,4 +92,21 @@ app.post("/api/createEvent",async (request,response)=>{
         response.status(500).json({msg:error})
     }
 })
+
+// route ajout event à un utilisateur
+app.put("/api/addEventToUser",async(request,response)=>{
+    // const filter = {_id : request.body.id}
+    // const update = {toto : 89}
+    let updates = request.body
+    updates = {
+        "id":updates.id,
+        "$addToSet":{"listEvents":updates.listEvents}
+    }
+    userModel.findOneAndUpdate({_id: request.body.id},updates,{new: true,upsert: true})
+        .then(updateUser => response.json(updateUser))
+        .catch(err => response.status(400).json("Error : "+err))
+})
+
+// route supprimer event à un utilisateur
+
 module.exports = app;
