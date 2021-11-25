bfe17fd0f80ac71c3aa475fd6f9baa2654f88069
Nicolas <nicolas.cuve@ynov.com>
2021-11-25 15:26:22 +0100

    fix/ delete list categoryModel

--- db/categories.js
+++ db/categories.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-    list:{
-        type: Array,
+    category:{
+        type: String,
         required:true
     }
 });
